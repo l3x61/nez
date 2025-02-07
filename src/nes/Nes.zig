ad2db742601fb35bed3b47b8c424e2c0efc454ce
l3x61 <me@l3x61.dev>
2025-02-07 21:15:24 +0100

    remove unused code

--- src/nes/Nes.zig
+++ src/nes/Nes.zig
@@ -4,13 +4,13 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const Cartridge = @import("Cartridge.zig");
-const Sram = @import("Sram.zig");
 
 cartridge: Cartridge,
-sram: Sram,
 
 pub fn init(allocator: Allocator) Nes {
-    return Nes{ .cartridge = Cartridge.init(allocator), .sram = Sram.init(std.crypto.random) };
+    return Nes{
+        .cartridge = Cartridge.init(allocator),
+    };
 }
 
 pub fn deinit(self: *Nes) void {
@@ -19,5 +19,4 @@ pub fn deinit(self: *Nes) void {
 
 pub fn draw(self: *Nes) !void {
     try self.cartridge.draw();
-    self.sram.draw();
 }
