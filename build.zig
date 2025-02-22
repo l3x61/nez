2711b6efab60eb5199f83be86b2554e69010a609
l3x61 <me@l3x61.dev>
2025-02-22 12:31:21 +0100

    typo

--- build.zig
+++ build.zig
@@ -1,58 +1,57 @@
 const std = @import("std");
+
+const assets_dir = "assets/";
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const exe_mod = b.createModule(.{
+
+    const exe = b.addExecutable(.{
+        .name = "nez",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const exe = b.addExecutable(.{
-        .name = "nez",
-        .root_module = exe_mod,
+
+    const zglfw = b.dependency("zglfw", .{});
+    exe.root_module.addImport("zglfw", zglfw.module("root"));
+    exe.linkLibrary(zglfw.artifact("glfw"));
+
+    const zopengl = b.dependency("zopengl", .{});
+    exe.root_module.addImport("zopengl", zopengl.module("root"));
+
+    const zgui = b.dependency("zgui", .{
+        .backend = .glfw_opengl3,
     });
+    exe.root_module.addImport("zgui", zgui.module("root"));
+    exe.linkLibrary(zgui.artifact("imgui"));
 
-    { // dependencies
-        const zglfw = b.dependency("zglfw", .{
-            .target = target,
-        });
-        exe.root_module.addImport("zglfw", zglfw.module("root"));
-        exe.linkLibrary(zglfw.artifact("glfw"));
+    const install_assets_step = b.addInstallDirectory(.{
+        .source_dir = b.path(assets_dir),
+        .install_dir = .{ .custom = "" },
+        .install_subdir = b.pathJoin(&.{ "bin", assets_dir }),
+    });
+    exe.step.dependOn(&install_assets_step.step);
 
-        const zopengl = b.dependency("zopengl", .{});
-        exe.root_module.addImport("zopengl", zopengl.module("root"));
-
-        const zgui = b.dependency("zgui", .{
-            .target = target,
-            .backend = .glfw_opengl3,
-        });
-
-        exe.root_module.addImport("zgui", zgui.module("root"));
-        exe.linkLibrary(zgui.artifact("imgui"));
-
-        const zosdialog = b.dependency("zosdialog", .{ .target = target });
-        exe.root_module.addImport("zosdialog", zosdialog.module("root"));
-        exe.linkLibrary(zosdialog.artifact("zosdialog"));
-
-        const install_assets_step = b.addInstallDirectory(.{
-            .source_dir = b.path("assets"),
-            .install_dir = .{ .custom = "" },
-            .install_subdir = b.pathJoin(&.{ "bin", "assets" }),
-        });
-        exe.step.dependOn(&install_assets_step.step);
-    }
     b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
+
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
+
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
